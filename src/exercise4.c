28436751fe186589e4ddd2cd9930889eab028709
Sargis-art <sargissargsan533@gmail.com>
2025-10-28 09:27:32 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SIZE 10
 
 int main() {
     float arr[SIZE];
+    char buffer[100];
     float X;
     int K;
-    int n = SIZE - 1; // количество изначальных элементов, чтобы хватило места для вставки
+    int n = SIZE - 1;
 
-    // Считаем n=SIZE-1 элементов
+    
     for (int i = 0; i < n; i++) {
         scanf("%f", &arr[i]);
     }
 
-    // Читаем X и K
-    scanf("%f %d", &X, &K);
-
-    // Проверяем корректность K
-    if (K < 0 || K > n) {
-        return 1; // ошибка
+   
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+       
+        if (sscanf(buffer, "%f %d", &X, &K) != 2) {
+            return 1; 
+        }
+    } else {
+        return 1; 
     }
 
-    // Сдвигаем элементы вправо, начиная с конца массива до K
+    
+    if (K < 0 || K > n) {
+        return 1;
+    }
+
+   
     for (int i = n; i > K; i--) {
         arr[i] = arr[i - 1];
     }
-
-    // Вставляем X в позицию K
     arr[K] = X;
 
-    // Выводим итоговый массив из SIZE элементов
+    
     for (int i = 0; i < SIZE; i++) {
         printf("%.2f ", arr[i]);
     }
