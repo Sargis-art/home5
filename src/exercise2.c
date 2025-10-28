341f9308b67f0abfe851e26cb99a9feea7426754
Sargis-art <sargissargsan533@gmail.com>
2025-10-28 09:05:41 +0700

    Update exercise2.c

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,21 @@
+
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    float arr[10];
+
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &arr[i]);
+    }
+
+
+    printf("\n");
+    for (int i = 9; i >= 0; i--) {
+        printf("%.2f ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
+
