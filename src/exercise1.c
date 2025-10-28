9241e83d46f8ba4a046e6f0b3238f103d4fbfbb2
Sargis-art <sargissargsan533@gmail.com>
2025-10-28 08:58:48 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    float arr[10];
+    float sum = 0.0f;
+
+
+    printf("Введите 10 вещественных чисел:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &arr[i]);
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        sum += arr[i];
+    }
+
+ 
+    float average = sum / 10;
+
+    printf("Среднее арифметическое: %.3f\n", average);
+
     return 0;
 }
